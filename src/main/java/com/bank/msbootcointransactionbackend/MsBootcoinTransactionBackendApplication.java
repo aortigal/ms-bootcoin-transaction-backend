1bcae3d9c8c7f5fb59ce1c688826ed01ba216cfb
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-29 19:28:41 -0500

    add estructure base

---
+++ src/main/java/com/bank/msbootcointransactionbackend/MsBootcoinTransactionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcointransactionbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBootcoinTransactionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBootcoinTransactionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/msbootcointransactionbackend/MsBootcoinTransactionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.msbootcointransactionbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBootcoinTransactionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
